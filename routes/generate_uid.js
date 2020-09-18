6504d115cd310b05ef8fb6543dc310edaa79b869
omryha <omry.hazut@mail.huji.ac.il>
2020-09-18 10:10:16 +0300

    Initial commit

---
+++ routes/generate_uid.js
@@ -0,0 +1,13 @@
+const express = require('express')
+var uid = require('uid-safe')
+var router = express.Router()
+
+/* GET a guid */
+router.get('/', (req, res, next) => {
+    var strUid = uid.sync(18)
+    res.json({
+        guid: strUid
+    })
+})
+
+module.exports = router
